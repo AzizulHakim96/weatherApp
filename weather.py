3d3735f9e2ea9d803166ae31744258a64cb6787c
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2021-02-28 04:16:50 -0600

    API setup done and fetched information and stored them in variables

--- weather.py
+++ weather.py
@@ -2,8 +2,27 @@ import tkinter as tk
 import requests
 import time
 
+def getWeather():
+    city = textfield.get()
+    api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=9d84da67061f14b850780301daeb66a4"
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    min_temp = int(json_data['main']['temp_min'] - 273.15)
+    max_temp = int(json_data['main']['temp_max'] - 273.15)
+    feels_like = int(json_data['main']['feels_like'] - 273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    windSpeed = json_data['wind']['speed']
+    sunrise = time.strftime("%H:%M:%S", time.gmtime(json_data['sys']['sunrise'] + 21600))
+    sunset = time.strftime("%H:%M:%S", time.gmtime(json_data['sys']['sunset'] + 21600))
+
+    final_info = condition + "\n" +str(temp) + "C"
+    final_data = "\n" + "Max Temp: " + str(max_temp) + "\n" + "Min Temp: " + str(min_temp)
+
+
 canvas = tk.Tk()
-canvas.size("600*500")
+canvas.geometry("600x500")
 canvas.title("Weather App")
 
 f = ("poppins", 15, "bold")
