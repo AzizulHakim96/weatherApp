edf9074c29dfa5c8852933ac26904bb35cd4bb5d
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2021-02-28 03:19:29 -0600

    UI setup

---
+++ weather.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import requests
+import time
+
+canvas = tk.Tk()
+canvas.size("600*500")
+canvas.title("Weather App")
+
+f = ("poppins", 15, "bold")
+t = ("poppins", 35, "bold")
+
+textfield = tk.Entry(canvas, font = t)
+textfield.pack(pady = 20)
+textfield.focus()
+
+label1 = tk.Label(canvas, font = t)
+label1.pack()
+label2 = tk.Label(canvas, font = f)
+label2.pack()
+
+canvas.mainloop()
+
